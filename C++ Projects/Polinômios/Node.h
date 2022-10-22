79de859a6a53c6b791bbaff046b2374a29d33b33
HugoNishimoto07 <89364149+HugoNishimoto07@users.noreply.github.com>
2022-10-21 21:19:41 -0300

    Create Node.h

---
+++ C++ Projects/Polinômios/Node.h
@@ -0,0 +1,14 @@
+#ifndef NODE_H_INCLUDED
+#define NODE_H_INCLUDED
+#include <string>
+using namespace std;
+
+class Node
+{
+public:
+    int coef;
+    int pot; 
+    Node *proximo; 
+
+};
+#endif // NODE_H_INCLUDED
