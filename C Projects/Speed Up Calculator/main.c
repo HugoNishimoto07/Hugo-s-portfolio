9b6a4e0ca082169046a87823ab56057720774d60
HugoNishimoto07 <89364149+HugoNishimoto07@users.noreply.github.com>
2022-10-21 21:12:53 -0300

    Create main.c

---
+++ C Projects/Speed Up Calculator/main.c
@@ -0,0 +1,43 @@
+#include <math.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+long double x = 1.0;
+long double aux = 0;
+
+
+long double *sum()
+{
+  
+  for (int i = 1; i <= 1000; i++)
+  {
+    aux = aux + (x/i);
+      
+  }
+
+  return &aux;
+}
+
+int main(void) 
+{
+  clock_t begin = clock();
+
+  pthread_t thread1;
+  
+
+  pthread_create(&thread1, NULL, sum, NULL);
+  pthread_join(thread1, NULL);
+  
+  printf("valor de X é : %Lf", aux);
+  
+  clock_t end = clock();
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+  printf("\n");
+
+  printf("Tempo de execução %f segundos", time_spent);
+
+
+  return 0;
+}
